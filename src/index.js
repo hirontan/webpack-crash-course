dcd433ed3790757d061ab06aa515dbdba5af3627
hirontan <hirontan@sora.flights>
2019-05-02 19:31:43 +0900

    implement css-loader and style-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash'
 // import { NAME, NiJou } from './utilities' // 自前の場合は、相対パス。JSの場合は拡張子は省略できる
 // import { NAME as NAME_OF_HAM } from './utilities' // 名前の別名として扱うときにもasを利用する
-import Tiger from './utilities' // デフォルトを利用してみる。デフォルトが指定されている場合は、クラス名と違っていても利用できる
+// import Tiger from './utilities' // デフォルトを利用してみる。デフォルトが指定されている場合は、クラス名と違っていても利用できる
 
-console.log(Tiger.say());
+// console.log(Tiger.say());
+
+import './style.css'
+// console.log(style.toString()) // toString関数を持っているので、利用するとCSSが出力できる
 
 function component() {
   const element = document.createElement('div')
@@ -13,3 +16,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.classList.add('haikei')
